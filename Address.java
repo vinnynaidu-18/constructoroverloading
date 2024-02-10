6250fd484b41c00f09435fdadc41fb97545c56ab
vinnynaidu-18 <vinnynaidu128@gmail.com>
2024-02-10 16:38:24 +0530

    hi vinny

--- Address.java
+++ Address.java
@@ -3,6 +3,18 @@ public class Address {
     public String streetName;
     public String pincode;
 
+    public String getCityName() {
+        return cityName;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
     public Address(String cityName, String streetName, String pincode) {
         this.cityName = cityName;
         this.streetName = streetName;
